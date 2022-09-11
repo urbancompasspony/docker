c1ce41b748f952ad01c90edfcbdc9596f8f23021
Nathan Drake <32694176+urbancompasspony@users.noreply.github.com>
2022-09-10 23:23:29 -0300

    Update AutoExport.sh

--- Custom_Images/migrate-domain/AutoExport.sh
+++ Custom_Images/migrate-domain/AutoExport.sh
@@ -13,12 +13,12 @@ chmod 777 -R  /var/lib/samba/sysvol/
 mkdir -p "$wd"/ACTIVE_DIRECTORY/cache
 mkdir -p "$wd"/ACTIVE_DIRECTORY/lib
 mkdir -p "$wd"/ACTIVE_DIRECTORY/log
-mkdir -p "$wd"/ACTIVE_DIRECTORY/etc
 mkdir -p "$wd"/ACTIVE_DIRECTORY/run
+mkdir -p "$wd"/ACTIVE_DIRECTORY/etc
 
 # Backup Folders:
 cp -Rf /var/cache/samba "$wd"/ACTIVE_DIRECTORY/cache/
 cp -Rf /var/lib/samba "$wd"/ACTIVE_DIRECTORY/lib/
 cp -Rf /var/log/samba "$wd"/ACTIVE_DIRECTORY/log/
-cp -Rf /etc/samba "$wd"/ACTIVE_DIRECTORY/etc/
 cp -Rf /run/samba "$wd"/ACTIVE_DIRECTORY/run/
+cp -Rf /etc "$wd"/ACTIVE_DIRECTORY/
