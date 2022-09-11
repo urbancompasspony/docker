af61b133057772dc441933dfcebee5640e35403b
Nathan Drake <32694176+urbancompasspony@users.noreply.github.com>
2022-09-10 22:50:41 -0300

    Update AutoExport.sh

--- Custom_Images/migrate-domain/AutoExport.sh
+++ Custom_Images/migrate-domain/AutoExport.sh
@@ -9,21 +9,21 @@ wd=$(pwd)
 
 service samba-ad-dc stop
 
-mkdir -p "$wd"/ACTIVE_DIRECTORY/cache
-mkdir -p "$wd"/ACTIVE_DIRECTORY/lib
-mkdir -p "$wd"/ACTIVE_DIRECTORY/log
-mkdir -p "$wd"/ACTIVE_DIRECTORY/etc
-mkdir -p "$wd"/ACTIVE_DIRECTORY/run
-mkdir -p "$wd"/ACTIVE_DIRECTORY/config
+mkdir -p "$wd"/ACTIVEDIRECTORY/cache
+mkdir -p "$wd"/ACTIVEDIRECTORY/lib
+mkdir -p "$wd"/ACTIVEDIRECTORY/log
+mkdir -p "$wd"/ACTIVEDIRECTORY/etc
+mkdir -p "$wd"/ACTIVEDIRECTORY/run
+mkdir -p "$wd"/ACTIVEDIRECTORY/config
 
 # Backup Folders:
-cp -Rf /var/cache/samba "$wd"/ACTIVE_DIRECTORY/cache/
-cp -Rf /var/lib/samba "$wd"/ACTIVE_DIRECTORY/lib/
-cp -Rf /var/log/samba "$wd"/ACTIVE_DIRECTORY/log/
-cp -Rf /etc/samba "$wd"/ACTIVE_DIRECTORY/etc/
-cp -Rf /run/samba "$wd"/ACTIVE_DIRECTORY/run/
+cp -Rf /var/cache/samba "$wd"/ACTIVEDIRECTORY/cache/
+cp -Rf /var/lib/samba "$wd"/ACTIVEDIRECTORY/lib/
+cp -Rf /var/log/samba "$wd"/ACTIVEDIRECTORY/log/
+cp -Rf /etc/samba "$wd"/ACTIVEDIRECTORY/etc/
+cp -Rf /run/samba "$wd"/ACTIVEDIRECTORY/run/
 
 # Backup Confs:
-cp -Rf /etc/hosts "$wd"/ACTIVE_DIRECTORY/config/
-cp -Rf /etc/krb5.conf "$wd"/ACTIVE_DIRECTORY/config/
-cp -Rf /etc/resolv.conf "$wd"/ACTIVE_DIRECTORY/config/
+cp -Rf /etc/hosts "$wd"/ACTIVEDIRECTORY/config/
+cp -Rf /etc/krb5.conf "$wd"/ACTIVEDIRECTORY/config/
+cp -Rf /etc/resolv.conf "$wd"/ACTIVEDIRECTORY/config/
