419005e4ee8f0495c248b7a852b59b0ddf55c115
Nathan Drake <32694176+urbancompasspony@users.noreply.github.com>
2022-09-11 14:40:41 -0300

    Update 07.1-AutoExport.sh

--- 07.1-AutoExport.sh
+++ 07.1-AutoExport.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+USERNAME="$1"
+
 [ "$EUID" -ne 0 ] && {
   echo "Run this script as Root!"
   exit
@@ -10,15 +12,17 @@ wd=$(pwd)
 service samba-ad-dc stop
 chmod 777 -R  /var/lib/samba/sysvol/
 
-mkdir -p "$wd"/ACTIVE_DIRECTORY/cache
-mkdir -p "$wd"/ACTIVE_DIRECTORY/lib
-mkdir -p "$wd"/ACTIVE_DIRECTORY/log
-mkdir -p "$wd"/ACTIVE_DIRECTORY/run
-mkdir -p "$wd"/ACTIVE_DIRECTORY/etc
+mkdir -p /home/"$USERNAME"/ACTIVE_DIRECTORY
+
+mkdir -p /home/"$USERNAME"/ACTIVE_DIRECTORY/cache
+mkdir -p /home/"$USERNAME"/ACTIVE_DIRECTORY/lib
+mkdir -p /home/"$USERNAME"/ACTIVE_DIRECTORY/log
+mkdir -p /home/"$USERNAME"/ACTIVE_DIRECTORY/run
+mkdir -p /home/"$USERNAME"/ACTIVE_DIRECTORY/etc
 
 # Backup Folders:
-cp -Rf /var/cache/samba "$wd"/ACTIVE_DIRECTORY/cache/
-cp -Rf /var/lib/samba "$wd"/ACTIVE_DIRECTORY/lib/
-cp -Rf /var/log/samba "$wd"/ACTIVE_DIRECTORY/log/
-cp -Rf /run/samba "$wd"/ACTIVE_DIRECTORY/run/
-cp -Rf /etc "$wd"/ACTIVE_DIRECTORY/
+cp -Rf /var/cache/samba /home/"$USERNAME"/ACTIVE_DIRECTORY/cache/
+cp -Rf /var/lib/samba /home/"$USERNAME"/ACTIVE_DIRECTORY/lib/
+cp -Rf /var/log/samba /home/"$USERNAME"/ACTIVE_DIRECTORY/log/
+cp -Rf /run/samba /home/"$USERNAME"/ACTIVE_DIRECTORY/run/
+cp -Rf /etc /home/"$USERNAME"/ACTIVE_DIRECTORY/
