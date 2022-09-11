2a670cd385c727785274271be3c18f48add8ef3e
Nathan Drake <32694176+urbancompasspony@users.noreply.github.com>
2022-09-10 23:20:50 -0300

    Update AutoExport.sh

--- Custom_Images/migrate-domain/AutoExport.sh
+++ Custom_Images/migrate-domain/AutoExport.sh
@@ -16,18 +16,9 @@ mkdir -p "$wd"/ACTIVE_DIRECTORY/log
 mkdir -p "$wd"/ACTIVE_DIRECTORY/etc
 mkdir -p "$wd"/ACTIVE_DIRECTORY/run
 
-mkdir -p "$wd"/ACTIVE_DIRECTORY/config/hosts
-mkdir -p "$wd"/ACTIVE_DIRECTORY/config/krb5
-mkdir -p "$wd"/ACTIVE_DIRECTORY/config/resolv
-
 # Backup Folders:
 cp -Rf /var/cache/samba "$wd"/ACTIVE_DIRECTORY/cache/
 cp -Rf /var/lib/samba "$wd"/ACTIVE_DIRECTORY/lib/
 cp -Rf /var/log/samba "$wd"/ACTIVE_DIRECTORY/log/
 cp -Rf /etc/samba "$wd"/ACTIVE_DIRECTORY/etc/
 cp -Rf /run/samba "$wd"/ACTIVE_DIRECTORY/run/
-
-# Backup Confs:
-cp -Rf /etc/hosts "$wd"/ACTIVE_DIRECTORY/config/hosts/
-cp -Rf /etc/krb5.conf "$wd"/ACTIVE_DIRECTORY/config/krb5/
-cp -Rf /etc/resolv.conf "$wd"/ACTIVE_DIRECTORY/config/resolv/
