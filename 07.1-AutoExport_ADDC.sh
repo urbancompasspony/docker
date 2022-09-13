0601a99c80fa18b5c76408918b20219896aa3b14
Nathan Drake <32694176+urbancompasspony@users.noreply.github.com>
2022-09-12 23:08:23 -0300

    Update 07.1-AutoExport_ADDC.sh

--- 07.1-AutoExport_ADDC.sh
+++ 07.1-AutoExport_ADDC.sh
@@ -11,17 +11,17 @@ service samba-ad-dc stop
 
 chmod 777 -R  /var/lib/samba/sysvol/
 
-mkdir -p /home/"$USERNAME"/ACTIVE_DIRECTORY
+mkdir -p /home/"$USERNAME"/.CONTAINERS/ACTIVE_DIRECTORY
 
-mkdir -p /home/"$USERNAME"/ACTIVE_DIRECTORY/cache
-mkdir -p /home/"$USERNAME"/ACTIVE_DIRECTORY/lib
-mkdir -p /home/"$USERNAME"/ACTIVE_DIRECTORY/log
-mkdir -p /home/"$USERNAME"/ACTIVE_DIRECTORY/run
-mkdir -p /home/"$USERNAME"/ACTIVE_DIRECTORY/etc
+mkdir -p /home/"$USERNAME"/.CONTAINERS/ACTIVE_DIRECTORY/cache
+mkdir -p /home/"$USERNAME"/.CONTAINERS/ACTIVE_DIRECTORY/lib
+mkdir -p /home/"$USERNAME"/.CONTAINERS/ACTIVE_DIRECTORY/log
+mkdir -p /home/"$USERNAME"/.CONTAINERS/ACTIVE_DIRECTORY/run
+mkdir -p /home/"$USERNAME"/.CONTAINERS/ACTIVE_DIRECTORY/etc
 
 # Backup Folders:
-cp -Rf /var/cache/samba /home/"$USERNAME"/ACTIVE_DIRECTORY/cache/
-cp -Rf /var/lib/samba /home/"$USERNAME"/ACTIVE_DIRECTORY/lib/
-cp -Rf /var/log/samba /home/"$USERNAME"/ACTIVE_DIRECTORY/log/
-cp -Rf /run/samba /home/"$USERNAME"/ACTIVE_DIRECTORY/run/
-cp -Rf /etc /home/"$USERNAME"/ACTIVE_DIRECTORY/
+cp -Rf /var/cache/samba /home/"$USERNAME"/.CONTAINERS/ACTIVE_DIRECTORY/cache/
+cp -Rf /var/lib/samba /home/"$USERNAME"/.CONTAINERS/ACTIVE_DIRECTORY/lib/
+cp -Rf /var/log/samba /home/"$USERNAME"/.CONTAINERS/ACTIVE_DIRECTORY/log/
+cp -Rf /run/samba /home/"$USERNAME"/.CONTAINERS/ACTIVE_DIRECTORY/run/
+cp -Rf /etc /home/"$USERNAME"/.CONTAINERS/ACTIVE_DIRECTORY/
