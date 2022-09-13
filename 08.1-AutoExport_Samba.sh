f6faa1573cb03d7d347bb94412ceb46b7a0f98f6
Nathan Drake <32694176+urbancompasspony@users.noreply.github.com>
2022-09-12 23:06:45 -0300

    Update 08.1-AutoExport_Samba.sh

--- 08.1-AutoExport_Samba.sh
+++ 08.1-AutoExport_Samba.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 USERNAME="$1"
+NOMESHARE="$2"
 
 [ "$EUID" -ne 0 ] && {
   echo "Run this script as Root!"
@@ -9,17 +10,17 @@ USERNAME="$1"
 
 service smbd stop; service nmbd stop
 
-mkdir -p /home/"$USERNAME"/SAMBA_SHARE
+mkdir -p /home/"$USERNAME"/.CONTAINERS/"$NOMESHARE"
 
-mkdir -p /home/"$USERNAME"/SAMBA_SHARE/cache
-mkdir -p /home/"$USERNAME"/SAMBA_SHARE/lib
-mkdir -p /home/"$USERNAME"/SAMBA_SHARE/log
-mkdir -p /home/"$USERNAME"/SAMBA_SHARE/run
-mkdir -p /home/"$USERNAME"/SAMBA_SHARE/etc
+mkdir -p /home/"$USERNAME"/.CONTAINERS/"$NOMESHARE"/cache
+mkdir -p /home/"$USERNAME"/.CONTAINERS/"$NOMESHARE"/lib
+mkdir -p /home/"$USERNAME"/.CONTAINERS/"$NOMESHARE"/log
+mkdir -p /home/"$USERNAME"/.CONTAINERS/"$NOMESHARE"/run
+mkdir -p /home/"$USERNAME"/.CONTAINERS/"$NOMESHARE"/etc
 
 # Backup Folders:
-cp -Rf /var/cache/samba /home/"$USERNAME"/SAMBA_SHARE/cache/
-cp -Rf /var/lib/samba /home/"$USERNAME"/SAMBA_SHARE/lib/
-cp -Rf /var/log/samba /home/"$USERNAME"/SAMBA_SHARE/log/
-cp -Rf /run/samba /home/"$USERNAME"/SAMBA_SHARE/run/
-cp -Rf /etc /home/"$USERNAME"/SAMBA_SHARE/
+cp -Rf /var/cache/samba /home/"$USERNAME"/.CONTAINERS/"$NOMESHARE"/cache/
+cp -Rf /var/lib/samba /home/"$USERNAME"/.CONTAINERS/"$NOMESHARE"/lib/
+cp -Rf /var/log/samba /home/"$USERNAME"/.CONTAINERS/"$NOMESHARE"/log/
+cp -Rf /run/samba /home/"$USERNAME"/.CONTAINERS/"$NOMESHARE"/run/
+cp -Rf /etc /home/"$USERNAME"/.CONTAINERS/"$NOMESHARE"/
