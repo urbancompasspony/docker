da6ac873567d557d706a7ee519b212f4a004497b
Nathan Drake <32694176+urbancompasspony@users.noreply.github.com>
2022-09-16 00:49:12 -0300

    Update 08.1-AutoExport_Samba.sh

--- 08.1-AutoExport_Samba.sh
+++ 08.1-AutoExport_Samba.sh
@@ -9,17 +9,17 @@ USERNAME="$1"
 
 service smbd stop; service nmbd stop
 
-mkdir -p /home/"$USERNAME"/.CONTAINERS/"$NOMESHARE"
+mkdir -p /srv/containers/SAMBA_SHARE
 
-mkdir -p /home/"$USERNAME"/.CONTAINERS/SAMBA_SHARE/cache
-mkdir -p /home/"$USERNAME"/.CONTAINERS/SAMBA_SHARE/lib
-mkdir -p /home/"$USERNAME"/.CONTAINERS/SAMBA_SHARE/log
-mkdir -p /home/"$USERNAME"/.CONTAINERS/SAMBA_SHARE/run
-mkdir -p /home/"$USERNAME"/.CONTAINERS/SAMBA_SHARE/etc
+mkdir -p /srv/containers/SAMBA_SHARE/cache
+mkdir -p /srv/containers/SAMBA_SHARE/lib
+mkdir -p /srv/containers/SAMBA_SHARE/log
+mkdir -p /srv/containers/SAMBA_SHARE/run
+mkdir -p /srv/containers/SAMBA_SHARE/etc
 
 # Backup Folders:
-cp -Rf /var/cache/samba /home/"$USERNAME"/.CONTAINERS/SAMBA_SHARE/cache/
-cp -Rf /var/lib/samba /home/"$USERNAME"/.CONTAINERS/SAMBA_SHARE/lib/
-cp -Rf /var/log/samba /home/"$USERNAME"/.CONTAINERS/SAMBA_SHARE/log/
-cp -Rf /run/samba /home/"$USERNAME"/.CONTAINERS/SAMBA_SHARE/run/
-cp -Rf /etc /home/"$USERNAME"/.CONTAINERS/SAMBA_SHARE/
+cp -Rf /var/cache/samba /srv/containers/SAMBA_SHARE/cache/
+cp -Rf /var/lib/samba /srv/containers/SAMBA_SHARE/lib/
+cp -Rf /var/log/samba /srv/containers/SAMBA_SHARE/log/
+cp -Rf /run/samba /srv/containers/SAMBA_SHARE/run/
+cp -Rf /etc /srv/containers/SAMBA_SHARE/
