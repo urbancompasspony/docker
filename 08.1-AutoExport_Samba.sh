f228067b7296e5a0753b1749a80ee901d84a5476
Nathan Drake <32694176+urbancompasspony@users.noreply.github.com>
2022-09-29 11:41:36 -0300

    Update 08.1-AutoExport_Samba.sh

--- 08.1-AutoExport_Samba.sh
+++ 08.1-AutoExport_Samba.sh
@@ -16,8 +16,8 @@ mkdir -p /srv/containers/SAMBA_SHARE/run
 mkdir -p /srv/containers/SAMBA_SHARE/etc
 
 # Backup Folders:
-cp -Rf /var/cache/samba /srv/containers/SAMBA_SHARE/cache/
-cp -Rf /var/lib/samba /srv/containers/SAMBA_SHARE/lib/
-cp -Rf /var/log/samba /srv/containers/SAMBA_SHARE/log/
-cp -Rf /run/samba /srv/containers/SAMBA_SHARE/run/
-cp -Rf /etc /srv/containers/SAMBA_SHARE/
+rsync -vah /var/cache/samba /srv/containers/SAMBA_SHARE/cache/
+rsync -vah /var/lib/samba /srv/containers/SAMBA_SHARE/lib/
+rsync -vah /var/log/samba /srv/containers/SAMBA_SHARE/log/
+rsync -vah /run/samba /srv/containers/SAMBA_SHARE/run/
+rsync -vah /etc /srv/containers/SAMBA_SHARE/
