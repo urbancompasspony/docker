08a1e486f049956cccbea453b636d03aecf52aa7
Nathan Drake <32694176+urbancompasspony@users.noreply.github.com>
2022-09-29 11:44:00 -0300

    Update 07.1-AutoExport_ADDC.sh

--- 07.1-AutoExport_ADDC.sh
+++ 07.1-AutoExport_ADDC.sh
@@ -18,8 +18,8 @@ mkdir -p /srv/containers/ACTIVE_DIRECTORY/run
 mkdir -p /srv/containers/ACTIVE_DIRECTORY/etc
 
 # Backup Folders:
-rsync -vah /var/cache/samba /srv/containers/ACTIVE_DIRECTORY/cache/
-rsync -vah /var/lib/samba /srv/containers/ACTIVE_DIRECTORY/lib/
-rsync -vah /var/log/samba /srv/containers/ACTIVE_DIRECTORY/log/
-rsync -vah /run/samba /srv/containers/ACTIVE_DIRECTORY/run/
-rsync -vah /etc /srv/containers/ACTIVE_DIRECTORY/
+rsync -vahH /var/cache/samba /srv/containers/ACTIVE_DIRECTORY/cache/
+rsync -vahH /var/lib/samba /srv/containers/ACTIVE_DIRECTORY/lib/
+rsync -vahH /var/log/samba /srv/containers/ACTIVE_DIRECTORY/log/
+rsync -vahH /run/samba /srv/containers/ACTIVE_DIRECTORY/run/
+rsync -vahH /etc /srv/containers/ACTIVE_DIRECTORY/
