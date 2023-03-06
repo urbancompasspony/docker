90ce3bef4ad85e100777e937c54ab5d1df121470
Nathan Drake <32694176+urbancompasspony@users.noreply.github.com>
2023-03-06 11:09:00 -0300

    Update entrypoint.sh

--- Custom_Images/reverseproxy/entrypoint.sh
+++ Custom_Images/reverseproxy/entrypoint.sh
@@ -5,14 +5,14 @@ echo "00 01 01 * * certbot --apache renew >> /var/log/cron.log 2>&1
 # This extra line makes it a valid cron" > scheduler.txt
 crontab scheduler.txt
 
-# CustomScript
+# Deploy Certs
 /script.sh &
 
 # Apache2
 /usr/sbin/apachectl start &
 
-# Cron as a service
+# Cron as a Service
 /usr/sbin/cron -f &
 
-# Block from exit
+# Keep container running!
 tail -f /dev/null
