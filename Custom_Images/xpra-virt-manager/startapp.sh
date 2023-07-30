f7311df4a63b3b423118a01a8d93d4bf57d7d17b
Nathan Drake <32694176+urbancompasspony@users.noreply.github.com>
2023-07-29 21:05:08 -0300

    Update startapp.sh

--- Custom_Images/xpra-virt-manager/startapp.sh
+++ Custom_Images/xpra-virt-manager/startapp.sh
@@ -4,7 +4,7 @@ dbus-launch gsettings set org.virt-manager.virt-manager.connections uris "$HOSTS
 dbus-launch gsettings set org.virt-manager.virt-manager.connections autoconnect "$HOSTS"
 dbus-launch gsettings set org.virt-manager.virt-manager xmleditor-enabled true
 
-xpra start --bind-tcp=0.0.0.0:$PORTS,auth=password:value=$passw0rd --html=on --start=virt-manager --daemon=no --xvfb="/usr/bin/Xvfb +extension Composite -screen 0 1360x768x24+32 -nolisten tcp -noreset" --pulseaudio=no --notifications=no --bell=no :100
+xpra start --bind-tcp=0.0.0.0:$PORTS,auth=password:value=$passw0rd --html=on --start=virt-manager --daemon=no --xvfb="/usr/bin/Xvfb +extension Composite -screen 0 "$resolution"x24+32 -nolisten tcp -noreset" --pulseaudio=no --notifications=no --bell=no :100
 
 trap 'exit 0' SIGTERM
 
