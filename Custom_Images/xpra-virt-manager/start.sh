5c710b382689bb3171760c259e081f9e4cb6bf06
Nathan Drake <32694176+urbancompasspony@users.noreply.github.com>
2023-07-29 22:04:42 -0300

    Update start.sh

--- Custom_Images/xpra-virt-manager/start.sh
+++ Custom_Images/xpra-virt-manager/start.sh
@@ -4,10 +4,12 @@ dbus-launch gsettings set org.virt-manager.virt-manager.connections uris "$HOSTS
 dbus-launch gsettings set org.virt-manager.virt-manager.connections autoconnect "$HOSTS"
 dbus-launch gsettings set org.virt-manager.virt-manager xmleditor-enabled true
 
-rm /tmp/.X100-lock
-
-xpra start --bind-tcp=0.0.0.0:$PORTS,auth=password:value=$passw0rd --html=on --start=virt-manager --daemon=no --xvfb="/usr/bin/Xvfb +extension Composite -screen 0 "$resolution"x24+32 -nolisten tcp -noreset" --pulseaudio=no --notifications=no --bell=no :100
-
 trap 'exit 0' SIGTERM
 
-while true; do sleep 1; done
+rm /tmp/.X100-lock
+
+while true;
+  do
+    xpra start --bind-tcp=0.0.0.0:$PORTS,auth=password:value=$passw0rd --html=on --start=virt-manager --daemon=no --xvfb="/usr/bin/Xvfb +extension Composite -screen 0 "$resolution"x24+32 -nolisten tcp -noreset" --pulseaudio=no --notifications=no --bell=no :100
+    sleep 5
+  done
