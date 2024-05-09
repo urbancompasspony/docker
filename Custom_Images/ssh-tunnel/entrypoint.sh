d5f995c60083250bdd1fb856cf0294dfb0a94a00
Zóio - The Green Eye <32694176+urbancompasspony@users.noreply.github.com>
2024-05-09 00:18:09 -0300

    Create entrypoint.sh

---
+++ Custom_Images/ssh-tunnel/entrypoint.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Start SSHd!
+exec /usr/sbin/sshd -D &
+
+# Some options to keep it clean
+scpoptions="-o StrictHostKeyChecking=accept-new -o UserKnownHostsFile=/dev/null -o ServerAliveInterval=60 -o ServerAliveCountMax=5"
+
+# Password pfSense
+var1=$(sed -n '1p' /data/ssh)
+
+# pfSense User
+var2=$(sed -n '2p' /data/ssh)
+
+# pfSense IP (Gateway)
+var3=$(sed -n '3p' /data/ssh)
+
+# New Port (default 2222)
+var4=$(sed -n '4p' /data/ssh)
+
+# Linux Server IP
+var5=$(sed -n '5p' /data/ssh)
+
+# Linux Server Port
+var6=$(sed -n '6p' /data/ssh)
+
+# Run key-gen to cleanup everything
+sshpass -p "$var1" ssh $scpoptions "$var2"@"$var3" -N -f -L "$var4":"$var5":"$var6"
+
+# Keep container running!
+tail -f /dev/null
+
+exit 0
