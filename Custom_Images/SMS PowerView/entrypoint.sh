c01efc720ddc048ea72c4e21927e9969f0c70d61
Zóio - The Green Eye <32694176+urbancompasspony@users.noreply.github.com>
2025-03-13 15:05:03 -0300

    Update entrypoint.sh

--- Custom_Images/SMS PowerView/entrypoint.sh
+++ Custom_Images/SMS PowerView/entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-/SMS/bin/powerview start -g --no-debug &
+trap 'exit 0' SIGTERM
 
-# Block container exit
-tail -f /dev/null
+rm /tmp/.X100-lock
+
+while true;
+  do
+    xpra start --bind-tcp=0.0.0.0:8080 --html=on --start="sudo ./SMS/bin/powerview start -g --no-debug" --daemon=no --xvfb="/usr/bin/Xvfb +extension Composite -screen 0 "$resolution"x24+32 -nolisten tcp -noreset" --pulseaudio=no --notifications=no --bell=no :100
+    sleep 5
+  done
