59df72344a6a0829e79aad74a742d9d7ea11a6b7
Zóio The Green Eye <32694176+urbancompasspony@users.noreply.github.com>
2025-07-04 09:32:53 -0300

    Update entrypoint.sh

--- Custom_Images/domain-teste/w apache/entrypoint.sh
+++ Custom_Images/domain-teste/w apache/entrypoint.sh
@@ -10,7 +10,14 @@
 /cups.sh &
 
 # APACHE SERVER com Samba CGI
+find /var/run/apache2/ -name "cgisock*" -exec unlink {} \; 2>/dev/null || true
+
 service apache2 start &
+
+if [ ! -S /var/run/apache2/cgisock ]; then
+  service apache2 restart
+  sleep 1
+fi
 
 # Block container exit
 tail -f /dev/null
